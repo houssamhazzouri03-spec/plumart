4eba937311315de21a2feccc34b8ed3c5eec20ce
houssamhazzouri03-spec <houssamhazzouri03@gmail.com>
2026-04-06 21:44:46 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.18,
+  }
+);
+
+reveals.forEach((item) => observer.observe(item));
